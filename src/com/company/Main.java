c030c26c5dadeb5ffcc99dfab4ddcacd49a0e171
gopala01 <bhamidipatig@gmail.com>
2021-12-25 12:49:42 +0000

    First try to the project commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,92 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi");
-        System.out.println("");
+        final int numJudges = 6;
+        final int numFilms = 2;
+
+        String[] filmNames = new String[numFilms];
+        int[] voteCounts = new int[numFilms];
+        int[] judgeNum = new int[numJudges];
+
+
+
+        filmNames = enter(filmNames);
+
+        voteCounts = voting(filmNames, voteCounts, judgeNum);
+
+        print("The winner is " + winner(filmNames,voteCounts));
+
+
+    }
+
+    public static String[] enter(String[] filmNames)
+    {
+        for (int i = 0; i < filmNames.length; i++) {
+            String filmName = inputString("What is the film name?");
+            filmNames[i] = filmName;
+        }
+
+        return filmNames;
+    }
+
+    public static int[] voting(String[] filmNames, int[] voteCounts, int[] judgeNum)
+    {
+        for (int i = 0; i < judgeNum.length; i++) {
+            int judge = inputInt("What is your judge number");
+            int arrayNum = judge-1;
+            try {
+                judgeNum[arrayNum]++;
+            }
+            catch (ArrayIndexOutOfBoundsException e)
+            {
+                judge = inputInt("Enter a proper name");
+            }
+        }
+
+
+        return voteCounts;
+    }
+
+
+    public static String winner(String[] filmNames, int[] voteCounts) {
+        String winningMovie = "";
+
+        for (int i = 0; i < voteCounts.length; i++) {
+            for (int j = 1; j < voteCounts.length; j++) {
+                if (voteCounts[j] > voteCounts[i]) {
+                    winningMovie = filmNames[j];
+                }
+            }
+        }
+        return winningMovie;
+    }
+
+    public static void print(String m)
+    {
+        System.out.println(m);
+    }
+
+    public static String inputString(String m)
+    {
+        String answer;
+        Scanner scanner = new Scanner(System.in);
+
+        print(m);
+        answer = scanner.nextLine();
+        return answer;
+    }
+
+    public static int inputInt(String m)
+    {
+        int answer;
+        Scanner scanner = new Scanner(System.in);
+
+        print(m);
+        answer = scanner.nextInt();
+        return answer;
     }
 }
