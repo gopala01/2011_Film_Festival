bef005961be1e90a6f1fd5f2d7e8c6c0e5e25451
gopala01 <bhamidipatig@gmail.com>
2021-12-26 21:59:58 +0000

    Final attempt commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,60 +5,77 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final int numJudges = 6;
-        final int numFilms = 2;
+        final int numJudges = 12;
+        final int numFilms = 5;
 
-        String[] filmNames = new String[numFilms];
-        int[] voteCounts = new int[numFilms];
-        int[] judgeNum = new int[numJudges];
+        String[] filmNameArray = new String[numFilms];
+        int[] voteCountArray = new int[numFilms];
+        int[] judgeNumArray = new int[numJudges];
 
 
 
-        filmNames = enter(filmNames);
+        filmNameArray = enter(filmNameArray);
 
-        voteCounts = voting(filmNames, voteCounts, judgeNum);
+        voteCountArray = voting(judgeNumArray, filmNameArray, voteCountArray);
 
-        print("The winner is " + winner(filmNames,voteCounts));
+        print("The winner is " + winner(filmNameArray,voteCountArray));
 
 
     }
 
-    public static String[] enter(String[] filmNames)
+    public static String[] enter(String[] filmNameArray)
     {
-        for (int i = 0; i < filmNames.length; i++) {
+        for (int i = 0; i < filmNameArray.length; i++) {
             String filmName = inputString("What is the film name?");
-            filmNames[i] = filmName;
+            filmNameArray[i] = filmName;
         }
 
-        return filmNames;
+        return filmNameArray;
     }
 
-    public static int[] voting(String[] filmNames, int[] voteCounts, int[] judgeNum)
+
+    public static int[] voting(int[] judgeNumArray, String[] filmNameArray, int[] voteCountArray)
     {
-        for (int i = 0; i < judgeNum.length; i++) {
-            int judge = inputInt("What is your judge number");
-            int arrayNum = judge-1;
-            try {
-                judgeNum[arrayNum]++;
-            }
-            catch (ArrayIndexOutOfBoundsException e)
+        for (int i = 0; i < judgeNumArray.length; i++) {
+            int numOfJudge = inputInt("What is your judge number?");
+            while (numOfJudge < 1 || numOfJudge > judgeNumArray.length)
             {
-                judge = inputInt("Enter a proper name");
+                numOfJudge = inputInt("Choose your judge number");
             }
+
+            while (judgeNumArray[numOfJudge-1] > 0)
+            {
+                numOfJudge = inputInt("Choose an unused judge number");
+            }
+            judgeNumArray[numOfJudge-1]++;
+            for (int j = 0; j < filmNameArray.length; j++) {
+                print((j+1) + " - " + filmNameArray[j]);
+            }
+            int choice = inputInt("What is your choice?");
+            while (choice < 1 || choice > filmNameArray.length+1)
+            {
+                choice = inputInt("Choose a number from the choices");
+            }
+            int numArray2 = choice -1;
+
+            voteCountArray[numArray2]++;
         }
-
-
-        return voteCounts;
+        return voteCountArray;
     }
 
 
     public static String winner(String[] filmNames, int[] voteCounts) {
-        String winningMovie = "";
+        String winningMovie = filmNames[0];
 
         for (int i = 0; i < voteCounts.length; i++) {
             for (int j = 1; j < voteCounts.length; j++) {
-                if (voteCounts[j] > voteCounts[i]) {
+                if (voteCounts[j] > voteCounts[i])
+                {
                     winningMovie = filmNames[j];
+                }
+                else
+                {
+                    winningMovie = filmNames[i];
                 }
             }
         }
